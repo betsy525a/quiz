63fd75897cc15502235b32179068f3211d2a5298
Betsabe Bajana <betsabe525@gmail.com>
2019-01-09 18:40:22 +0000

    draft

--- js/script.js
+++ js/script.js
@@ -2,19 +2,111 @@
 
 $(document).ready(function() {
     $("button").click(function() {
-        var name = $("#name").val();
         var techPlacement = "TBD";
         var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
-        var q1Result = $("#question1").val();
-        var q2Result = $("#question2").val();
-        var q3Result = $("#question3").val();
+        var q1Result = $("input[name='question1']:checked").val();
+        var q2Result = $("input[name='question2']:checked").val();
+        var q3Result = $("input[name='question3']:checked").val();
+        var q4Result = $("input[name='question4']:checked").val();
         var totalScore;
     });
-
-    $("#result1").click(function() {
-       var selectedValue = $("input[name='1']:checked").val(); 
-       $("#result11").text(selectedValue);
-    });
+    
     
 });
 
+/*
+Answers
+
+Williams: WI
+Pink
+No workouts
+Math
+Threaten Them
+Go get lunch, move car
+
+Q: QU
+Blue
+No workouts but gym membership
+English
+Fail them
+Deal with seniors
+Basement Bathroom
+3 hours of sleep
+3:34 but try to leave as soon as possible
+
+Tancreto: TA
+Purple
+When I'm not pregnant
+English
+Laugh or talk to them in a not so nice way to make them aware
+I don't have any
+4th floor
+6-7 hours
+5pm
+Chicken Wings
+
+Lewis: LE
+Hot pink
+no workout *Laughs histerically*
+Math
+Yell and call home and fail
+Listen to music and plan for class
+2nd floor and lock the door behind me
+Not enough - 5 to 6 hours on weekdays
+2:30 or 4:35 - 5pm depending on the day
+Mom's cooking
+
+Mobley: MO
+No color
+often
+Chemistry
+Help them during office hours
+deal with other people's problems
+4th bathroom
+5 hours of sleep
+6:30pm
+
+Betancourt: BE
+Purple
+Workout 6 days a week
+ELA
+No extra credit / get upset
+Grade, workout, eat, go to the store
+4th floor
+not enough 6-7
+Usually 3:30-4pm
+
+Carmona: CA
+Blue
+3 times a week
+Spanish
+Call home
+Look up more assignments and projects for students
+3rd floor
+5 hours
+6pm
+spanish food
+spanish/cheerful/groovy
+scarface
+cookie from empire
+phone
+to be able to change people's way of thinking or mind control
+Give 10% to church and then buy a huge house for orphans
+
+Rivera: RI1
+Red 
+Every day
+Math
+Talk to students first and then call home
+Work
+4th floor
+6 hours
+4:30
+Pizza
+Top 40 pop
+Dead poet society
+Daffy Duck
+Matches
+Invisibility
+Invest money into a retirement plan
+*/
