20d83ac18a8c4d9576613a7a05004d0aa747c330
Betsabe Bajana <betsabe525@gmail.com>
2019-01-15 18:41:05 +0000

    second draft

--- js/script.js
+++ js/script.js
@@ -1,16 +1,373 @@
 /* global $ */
 
 $(document).ready(function() {
+    
+    var williams = 0;
+    var quinones = 0;
+    var tancreto = 0;
+    var lewis = 0;
+    var mobley = 0;
+    var betancourt = 0;
+    var carmona = 0;
+    var msRivera = 0;
+    var echChaouy = 0;
+    var hershinson = 0;
+    var mrRivera = 0;
+    var cohen = 0;
+        
+    
     $("button").click(function() {
-        var techPlacement = "TBD";
-        var imgSrc = "https://media.giphy.com/media/22zgHX8aop488/giphy.gif";
+        //for test. delete this later
+        $("#test").text("");
+        williams = 0;
+        quinones = 0;
+        tancreto = 0;
+        lewis = 0;
+        mobley = 0;
+        betancourt = 0;
+        carmona = 0;
+        msRivera = 0;
+        echChaouy = 0;
+        hershinson = 0;
+        mrRivera = 0;
+        cohen = 0;
+        //end of test
         var q1Result = $("input[name='question1']:checked").val();
         var q2Result = $("input[name='question2']:checked").val();
         var q3Result = $("input[name='question3']:checked").val();
         var q4Result = $("input[name='question4']:checked").val();
-        var totalScore;
+        var q5Result = $("input[name='question5']:checked").val();
+        var q6Result = $("input[name='question6']:checked").val();
+        var q7Result = $("input[name='question7']:checked").val();
+        var q8Result = $("input[name='question8']:checked").val();
+        var q9Result = $("input[name='question9']:checked").val();
+        var q10Result = $("input[name='question10']:checked").val();
+        var q11Result = $("input[name='question11']:checked").val();
+        var q12Result = $("input[name='question12']:checked").val();
+        var q13Result = $("input[name='question13']:checked").val();
+        var q14Result = $("input[name='question14']:checked").val();
+        var q15Result = $("input[name='question15']:checked").val();
+        
+        question1(q1Result);
+        question2(q2Result);
+        question3(q3Result);
+        question4(q4Result);
+        question5(q5Result);
+        question6(q6Result);
+        question7(q7Result);
+        question8(q8Result);
+        question9(q9Result);
+        question10(q10Result);
+        question11(q11Result);
+        question12(q12Result);
+        question13(q13Result);
+        question14(q14Result);
+        question15(q15Result);
+        
+        $("#test").append("<p>The score for Williams is " + williams + "</p>");
+        $("#test").append("<p>The score for Quinones is " + quinones + "</p>");
+        $("#test").append("<p>The score for Tancreto is " + tancreto + "</p>");
+        $("#test").append("<p>The score for Lewis is " + lewis + "</p>");
+        $("#test").append("<p>The score for Mobley is " + mobley + "</p>");
+        $("#test").append("<p>The score for Betancourt is " + betancourt + "</p>");
+        $("#test").append("<p>The score for Carmona is " + carmona + "</p>");
+        $("#test").append("<p>The score for Ms. Rivera is " + msRivera + "</p>");
+        $("#test").append("<p>The score for EchChaouy is " + echChaouy + "</p>");
+        $("#test").append("<p>The score for Hershinson is " + hershinson + "</p>");
+        $("#test").append("<p>The score for Mr. Rivera is " + mrRivera + "</p>");
+        $("#test").append("<p>The score for Cohen is " + cohen + "</p>");
     });
     
+    function question1(userPick) {
+        if(userPick === "QU & CA & MrR"){
+            quinones = quinones + 1;
+            carmona = carmona + 1;
+            mrRivera = mrRivera + 1;
+        }
+        else if(userPick === "TA & BE & CO"){
+            tancreto = tancreto + 1;
+            betancourt = betancourt + 1;
+            cohen = cohen + 1;
+        }
+        else {
+            tallyUp1(userPick);
+        }
+    }
+    
+    function question2(userPick) {
+        if(userPick === "MsR & EC & CO"){
+            msRivera = msRivera + 1;
+            echChaouy = echChaouy + 1;
+            cohen = cohen + 1;
+        }
+        else {
+            tallyUp1(userPick);
+        }
+    }
+    
+    function question3(userPick) {
+        if(userPick === "WI & LE & MsR & CO"){
+            williams = williams + 1;
+            lewis = lewis + 1;
+            msRivera = msRivera + 1;
+            cohen = cohen + 1;
+        }
+        else if(userPick === "QU & TA & BE"){
+            quinones = quinones + 1;
+            tancreto = tancreto + 1;
+            betancourt = betancourt + 1;
+        }
+        else{
+            tallyUp1(userPick);
+        }
+    }
+    
+    function question4(userPick) {
+        if(userPick === "QU & HE & LE"){
+            quinones = quinones + 1;
+            hershinson = hershinson + 2;
+            lewis = lewis + 1;
+        }
+        else if(userPick === "LE & CA & MsR"){
+            lewis = lewis + 1;
+            carmona = carmona + 2;
+            msRivera = msRivera + 1;
+        }
+        else if(userPick === "MO & BE"){
+            mobley = mobley + 1;
+            betancourt = betancourt + 1;
+        }
+        else if(userPick === "EC & CO"){
+            echChaouy = echChaouy + 2;
+            cohen = cohen + 2;
+        }
+        else {
+            tallyUp2(userPick);
+        }
+    }
+    
+    function question5(userPick) {
+        if(userPick === "WI & BE"){
+            williams = williams + 1;
+            betancourt = betancourt + 1;
+        }
+        else if(userPick === "LE & EC & MrR"){
+            lewis = lewis + 1;
+            echChaouy = echChaouy + 2;
+            mrRivera = mrRivera + 1;
+        }
+        else if(userPick === "BE & CO"){
+            cohen = cohen + 2;
+            betancourt = betancourt + 1;
+        }
+        else if(userPick === "BE & HE & CO"){
+            betancourt = betancourt + 1;
+            hershinson = hershinson + 1;
+            cohen = cohen + 1;
+        }
+        else if(userPick === "MrR & HE"){
+            mrRivera = mrRivera + 1;
+            hershinson = hershinson + 1;
+        }
+        else {
+            tallyUp2(userPick);
+        }
+    }
+    
+    function question6(userPick) {
+        if(userPick === "QU & MrR"){
+            quinones = quinones + 1;
+            mrRivera = mrRivera + 1;
+        }
+        else if(userPick === "CA & EC & HE"){
+            carmona = carmona + 1;
+            echChaouy = echChaouy + 1;
+            hershinson = hershinson + 1;
+        }
+        else if(userPick === "WI & TA & MO & BE & MsR"){
+            mobley = mobley + 1;
+            betancourt = betancourt + 1;
+            williams = williams + 1;
+            tancreto = tancreto + 1;
+            msRivera = msRivera + 1;
+        }
+        else {
+            tallyUp1(userPick);
+        }
+    }
+    
+    function question7(userPick) {
+        if(userPick === "MO & CA & CO"){
+            mobley = mobley + 1;
+            carmona = carmona + 1;
+            cohen = cohen + 1;
+        }
+        else if(userPick === "TA & BE"){
+            tancreto = tancreto + 1;
+            betancourt = betancourt + 1;
+        }
+        else if(userPick === "WI & MsR & MrR"){
+            williams = williams + 1;
+            msRivera = msRivera + 1;
+            mrRivera = mrRivera + 1;
+        }
+        else {
+        tallyUp1(userPick);
+        }
+    }
+    
+    function question8(userPick) {
+        if(userPick === "WI & BE"){
+            williams = williams + 1;
+            betancourt = betancourt + 1;
+        }
+        else if(userPick === "TA & MrR"){
+            mrRivera = mrRivera + 1;
+            tancreto = tancreto + 1;
+        }
+        else if(userPick === "EC & HE"){
+            echChaouy = echChaouy + 1;
+            hershinson = hershinson + 1;
+        }
+        else {
+            tallyUp1(userPick);
+        }
+    }
+    
+    function question9(userPick) {
+        if(userPick === "MsR & HE"){
+            msRivera = msRivera + 1;
+            hershinson = hershinson + 1;
+        }
+        else {
+            tallyUp1(userPick);
+        }
+    }
+    
+    function question10(userPick) {
+        tallyUp2(userPick);
+    }
+    
+    function question11(userPick) {
+        tallyUp2(userPick);
+    }
+    
+    function question12(userPick) {
+        if(userPick === "EC & CO"){
+            echChaouy = echChaouy + 1;
+            cohen = cohen + 1;
+        }
+        else {
+            tallyUp1(userPick);
+        }
+    }
+    
+    function question13(userPick) {
+        tallyUp2(userPick);
+    }
+    
+    function question14(userPick) {
+        if(userPick === "MO & CO"){
+            mobley = mobley + 1;
+            cohen = cohen + 1;
+        }
+        else {
+            tallyUp1(userPick);
+        }
+    }
+    
+    function question15(userPick) {
+        if(userPick === "WI & HE"){
+            williams = williams + 1;
+            hershinson = hershinson + 1;
+        }
+        else if(userPick === "QU & CO"){
+            quinones = quinones + 1;
+            cohen = cohen + 1;
+        }
+        else {
+            tallyUp1(userPick);
+        }
+    }
+    
+    
+    function tallyUp1(userChoice) {
+        if(userChoice === "WI") {
+            williams = williams + 1;
+        }
+        else if(userChoice === "QU") {
+            quinones = quinones + 1;
+        }
+        else if(userChoice === "TA") {
+            tancreto = tancreto + 1;
+        }
+        else if(userChoice === "LE") {
+            lewis = lewis + 1;
+        }
+        else if(userChoice === "BE") {
+            betancourt = betancourt + 1;
+        }
+        else if(userChoice === "MO") {
+            mobley = mobley + 1;
+        }
+        else if(userChoice === "CA") {
+            carmona = carmona + 1;
+        }
+        else if(userChoice === "MsR") {
+            msRivera = msRivera + 1;
+        }
+        else if(userChoice === "EC") {
+            echChaouy = echChaouy + 1;
+        }
+        else if(userChoice === "HE") {
+            hershinson = hershinson + 1;
+        }
+        else if(userChoice === "MrR") {
+            mrRivera = mrRivera + 1;
+        }
+        else if(userChoice === "CO") {
+            cohen = cohen + 1;
+        }
+    }
+    
+    function tallyUp2(userChoice) {
+        if(userChoice === "WI") {
+            williams = williams + 2;
+        }
+        else if(userChoice === "QU") {
+            quinones = quinones + 2;
+        }
+        else if(userChoice === "TA") {
+            tancreto = tancreto + 2;
+        }
+        else if(userChoice === "LE") {
+            lewis = lewis + 2;
+        }
+        else if(userChoice === "BE") {
+            betancourt = betancourt + 2;
+        }
+        else if(userChoice === "MO") {
+            mobley = mobley + 2;
+        }
+        else if(userChoice === "CA") {
+            carmona = carmona + 2;
+        }
+        else if(userChoice === "MsR") {
+            msRivera = msRivera + 2;
+        }
+        else if(userChoice === "EC") {
+            echChaouy = echChaouy + 2;
+        }
+        else if(userChoice === "HE") {
+            hershinson = hershinson + 2;
+        }
+        else if(userChoice === "MrR") {
+            mrRivera = mrRivera + 2;
+        }
+        else if(userChoice === "CO") {
+            cohen = cohen + 2;
+        }
+    }
     
 });
 
@@ -23,6 +380,16 @@ No workouts
 Math
 Threaten Them
 Go get lunch, move car
+4th floor bathroom
+6 hours
+4pm
+Italian
+whatever on the radio
+Willy Wonka and the chocolate factory
+Kara Danvers aka Supergirl
+My bed
+Teleport
+A house
 
 Q: QU
 Blue
@@ -33,6 +400,13 @@ Deal with seniors
 Basement Bathroom
 3 hours of sleep
 3:34 but try to leave as soon as possible
+Eggplant Parmigiana
+90's Rap or 90's alternative rock
+Pulp fiction
+Seth Rogen
+A boat
+Control Metal
+Put half a million towards his kids education
 
 Tancreto: TA
 Purple
@@ -55,16 +429,29 @@ Listen to music and plan for class
 Not enough - 5 to 6 hours on weekdays
 2:30 or 4:35 - 5pm depending on the day
 Mom's cooking
+Christian classical, reg, hip
+Brown Sugar
+Beyonce, Michelle Obama
+A phone to call my husband so he can get me out of there
+Invisibility
+Plane tickets
 
 Mobley: MO
 No color
 often
 Chemistry
-Help them during office hours
+Help them during office hours // no extra credit
 deal with other people's problems
 4th bathroom
 5 hours of sleep
 6:30pm
+Seafood
+Rap
+300
+Taraji P. Henson
+A male and female cow
+Flying
+Buy mom a house
 
 Betancourt: BE
 Purple
@@ -93,7 +480,7 @@ phone
 to be able to change people's way of thinking or mind control
 Give 10% to church and then buy a huge house for orphans
 
-Rivera: RI1
+Rivera: MsR
 Red 
 Every day
 Math
@@ -109,4 +496,72 @@ Daffy Duck
 Matches
 Invisibility
 Invest money into a retirement plan
+
+EchChaouy: EC
+Green
+Everyday
+Global
+Try to help them
+Prepare
+3rd floor
+7 hours
+2:35pm
+Couscous
+Moroccan
+Casablanca
+Nobody
+Fishing rod
+See the future
+Give it away to charity
+
+Hershinson: HE
+Turquoise
+Not that often - 1 time a week
+History
+Fail them
+Grade things and deal with students
+3rd floor bathroom
+6.5 hours
+2:35pm
+Pizza
+EDM
+Hackers
+Young Jessica Biel
+A satellite phone
+Time travel
+A house
+
+Rivera: MrR
+Blue
+Try to
+Science
+Find out why they fail and fix it
+Prepare for class and meet with students
+Basement
+6 hours
+Average 5pm
+Rice, beans, and chicken
+Alternative rock
+The Godfather
+Brad Pitt
+iPad with cellular data
+Superman's powers
+Down payment on a house
+
+Cohen: CO
+Purple
+Everday
+Math
+Try to help them pass
+Workout or grade papers
+RiseBoro side
+5 hours
+6:15pm
+Thai food
+Everything except for country music
+The Shawshank Redemption
+Nobody
+Toothbrush
+Flying
+Kids College
 */
